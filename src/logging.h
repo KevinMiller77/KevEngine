d306c1c8648b43cabbccf590def2a5475064ea8f
Adin W-T <adinrwt@gmail.com>
2019-07-15 22:41:50 -0700

    Added a console to the win32 program, added logging.h and improved logging.h
    Used AllocConsole() to allocate a console for the program as one isn't provided
    when starting from WinMain().  After this the three standard crt streams are redirected
    to the new console.  Additionally logging.h was refactored to have one universal LOG()
    interface and support format parameters to printf.

---
+++ src/logging.h
@@ -0,0 +1,56 @@
+#ifndef __TLETC_LOGGING__
+#define __TLETC_LOGGING__
+
+#define DEBUG_LEVEL 4   //(Debugging level; 1) Error  2) Warning 3) Info
+//                  4) Debug 5) Verbose 6)Too Much Info
+
+#ifdef DEBUG_LEVEL
+#define LOG(type, ...) LOG_##type(__VA_ARGS__)
+#else
+#define LOG(type, ...)
+#endif
+
+//Error Log
+#if DEBUG_LEVEL > 0
+#define LOG_ERR(...) printf("[TLETC] E: " __VA_ARGS__)
+#else
+#define LOG_ERR(...)
+#endif
+
+//Warning Log
+#if DEBUG_LEVEL > 1
+#define LOG_WRN(...) printf("[TLETC] W: " __VA_ARGS__)
+#else
+#define LOG_WRN(...)
+#endif
+
+//Info Log
+#if DEBUG_LEVEL > 2
+#define LOG_INF(...) printf("[TLETC] I: " __VA_ARGS__)
+#else
+#define LOG_INF(...)
+#endif
+
+//Debug Log
+#if DEBUG_LEVEL > 3
+#define LOG_DBG(...) printf("[TLETC] D: " __VA_ARGS__)
+#else
+#define LOG_DBG(...)
+#endif
+
+//Verbose Log
+#if DEBUG_LEVEL > 4
+#define LOG_VER(...)  printf("[TLETC] V: " __VA_ARGS__)
+#else
+#define LOG_VER(...)
+#endif
+
+//TMI Log
+#if DEBUG_LEVEL > 5
+#define LOG_TMI(...) printf("[TLETC] TMI: " __VA_ARGS__)
+#else
+#define LOG_TMI(...)
+#endif
+
+
+#endif
