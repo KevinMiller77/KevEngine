77ba03c78f69a2cb7ea235db7d535cb1c838c898
Kevin Miller <kevinamiller77@gmail.com>
2020-03-19 21:47:22 -0400

    add glfw

--- ext/freetype-gl/opengl.h
+++ ext/freetype-gl/opengl.h
@@ -33,18 +33,6 @@
  */
 #ifndef __OPEN_GL_H__
 #define __OPEN_GL_H__
-
-#if defined(__APPLE__)
-#  ifdef GL_ES_VERSION_2_0
-#    include <OpenGLES/ES2/gl.h>
-#  else
-#    include <OpenGL/gl.h>
-#  endif
-#elif defined(_WIN32) || defined(_WIN64)
-#  include <GL/glew.h>
-#else
-#  include <GL/glew.h>
-#  include <GL/gl.h>
-#endif
+#include <glad/glad.h>
 
 #endif /* OPEN_GL_H */
