cf58e7032f9f34da769a5fd6e1ca18e03a7b68e7
Kevin Miller <kevinamiller77@gmail.com>
2020-05-02 12:30:56 -0400

    Gitignore

---
+++ src/graphics/objects/KevObject.c
@@ -0,0 +1,8 @@
+//
+//  KevObject.c
+//  Engine
+//
+//  Created by Kevin Miller on 5/1/20.
+//
+
+#include "KevObject.h"
---
+++ src/graphics/objects/attributes/KevAttribute.hpp
@@ -0,0 +1,13 @@
+//
+//  KevAttribute.hpp
+//  Engine
+//
+//  Created by Kevin Miller on 5/1/20.
+//
+
+#ifndef KevAttribute_hpp
+#define KevAttribute_hpp
+
+#include <stdio.h>
+
+#endif /* KevAttribute_hpp */
